98105cc05d6267658898925e4aceda0643b678fa
ex0dus-0x <ex0dus@codemuch.tech>
2017-12-17 23:35:32 -0500

    Started code for PPH library

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ Implementation of the PolyPasswordHasher password storage scheme in Rust
 ## TODO
 
 * [ ] Write more tests for SSS library
+* [ ] FFI link with `fastpolymath.c` library
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,43 @@
 extern crate ring;
 
 mod shamirsecret;
+
+pub struct PolyPasswordHasher {
+    accountdict: Option<String>,
+    shamirsecretobj: Option<ShamirSecret>,
+    knownsecret: bool,
+    saltsize: u8,
+    hasher: // TODO: AES type
+    serializer: // TODO: Serializer type
+    partialbytes: u8,
+    thresholdsecretkey: Option<>,
+    nextavailableshare: u8
+}
+
+impl Default for PolyPasswordHasher {
+    fn default() -> PolyPasswordHasher {
+            
+    }
+}
+
+impl PolyPasswordHasher {
+    pub fn new(threshold: u8, passwordfile: Option<String>, _partialbytes: Option<u8>) -> PolyPasswordHasher {
+        
+        let mut partialbytes: u8 = 0;
+        
+        // Provide default case for partialbytes variable
+        if let Some(p) = _partialbytes {
+            partialbytes = p;
+        }
+        
+        
+        
+    }
+    
+}
+
+fn do_bytearray_xor(a, b) {
+    if a.len() != b.len() {
+        
+    }
+}
